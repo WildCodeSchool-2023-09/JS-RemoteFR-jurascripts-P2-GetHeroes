7f4b4d44ce2258793e797c6a9bf7c39719082011
Blackstars <nelson.64@live.fr>
2023-10-28 01:23:50 +0200

    router page de jeux diparution du bouton au clic

--- frontend/src/components/bouton_play/BoutonPlay.jsx
+++ frontend/src/components/bouton_play/BoutonPlay.jsx
@@ -1,12 +1,21 @@
+import PropTypes from "prop-types";
 import "./boutonPlay.scss";
 import imgPlay from "../../assets/images/Play_game.svg";
 
-function BoutonPlay() {
+function BoutonPlay({ playAfficher }) {
   return (
     <section className="btnPlay">
-      <img src={imgPlay} alt="Play" />
+      <img
+        className={playAfficher ? "actif" : "noActif"}
+        src={imgPlay}
+        alt="Play"
+      />
     </section>
   );
 }
+
+BoutonPlay.propTypes = {
+  playAfficher: PropTypes.bool.isRequired,
+};
 
 export default BoutonPlay;

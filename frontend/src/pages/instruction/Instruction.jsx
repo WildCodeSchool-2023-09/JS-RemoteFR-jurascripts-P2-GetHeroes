8cb4cfd9bd0fc2c7ceb1ed932c77a5f4c4e5526d
Blackstars <nelson.64@live.fr>
2023-10-29 00:22:53 +0200

    creer route dans main finis scss le jsx manque les instruction

---
+++ frontend/src/pages/instruction/Instruction.jsx
@@ -0,0 +1,20 @@
+import "./instruction.scss";
+
+function Instruction() {
+  return (
+    <section className="contenairInstruction">
+      <h2>Instruction</h2>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum quidem
+        maxime ipsum aspernatur perferendis, neque nemo vel optio ut eos
+        suscipit nobis veritatis, corrupti consequuntur sit eligendi. Mollitia,
+        consectetur adipisci! Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Corrupti maxime saepe quis reiciendis, nesciunt laborum quas, quos
+        voluptatem nemo, pariatur facere distinctio quisquam eos consequatur?
+        Facilis odio officia beatae quidem.
+      </p>
+    </section>
+  );
+}
+
+export default Instruction;

f77e259402f141d57d0f3918733b4b79a9a686ab
Dricks45 <144217492+Dricks45@users.noreply.github.com>
2023-10-28 10:51:31 +0200

    Ajout du titre

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Token from "./components/token/Token";
 import BoutonPlay from "./components/bouton_play/BoutonPlay";
 import Footer from "./components/footer/Footer";
+import Titre from "./components/titre/Titre";
 
 function App() {
   // eslint-disable-next-line no-unused-vars
@@ -18,6 +19,7 @@ function App() {
         <Token token={token} />
       </header>
       <main>
+        <Titre />
         <BoutonPlay />
       </main>
       <footer>

9502469b5cb8abc77d261168dc4c422c9758a0cd
Blackstars <nelson.64@live.fr>
2023-10-29 02:16:21 +0200

    ajout des instructions + scss

--- frontend/src/pages/instruction/Instruction.jsx
+++ frontend/src/pages/instruction/Instruction.jsx
@@ -4,14 +4,23 @@ function Instruction() {
   return (
     <section className="contenairInstruction">
       <h2>Instruction</h2>
+      <p>Le but du jeu est de trouver ton héros le plus vite possible.</p>
       <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum quidem
-        maxime ipsum aspernatur perferendis, neque nemo vel optio ut eos
-        suscipit nobis veritatis, corrupti consequuntur sit eligendi. Mollitia,
-        consectetur adipisci! Lorem ipsum dolor sit amet consectetur adipisicing
-        elit. Corrupti maxime saepe quis reiciendis, nesciunt laborum quas, quos
-        voluptatem nemo, pariatur facere distinctio quisquam eos consequatur?
-        Facilis odio officia beatae quidem.
+        Le bouton Révéler vous permet de dévoiler le héros. Mais plus vous
+        révélerez le héros, moins vous gagnerez de Tokens.
+      </p>
+      <p>
+        Quand vous avez deviné le héros, écrivez-le et appuyez sur le bouton
+        valider. Si c'est le bon héros, vous gagnerez la carte du héros, mais
+        aussi des token en fonction du nombre de fois que vous aurez pu le
+        révéler.
+      </p>
+      <p>
+        Vous pourrez aussi acheter des cartes des héros dans votre collection
+        grâce aux tokens.
+      </p>
+      <p className="last">
+        Bonne partie et trouver le héros que vous appréciez le plus.
       </p>
     </section>
   );

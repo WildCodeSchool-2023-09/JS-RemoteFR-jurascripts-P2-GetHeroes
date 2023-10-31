185f148b3d55e782017c108503b31ae1a119faa1
Blackstars <nelson.64@live.fr>
2023-10-31 18:08:11 +0100

    image a gratté plus imge test et dependence

--- frontend/src/pages/en_jeux/EnJeux.jsx
+++ frontend/src/pages/en_jeux/EnJeux.jsx
@@ -1,5 +1,4 @@
 import EnJeuxCarte from "../../components/page_de_jeux/en_jeux_carte/EnJeuxCarte";
-import RevelationCarte from "../../components/page_de_jeux/revelation_carte/RevelationCarte";
 import Solution from "../../components/page_de_jeux/solution/Solution";
 
 function EnJeux() {
@@ -7,9 +6,6 @@ function EnJeux() {
     <section className="contenairPageJeux">
       <section className="contenairCarteJeux">
         <EnJeuxCarte />
-      </section>
-      <section className="contenairRevelationCarte">
-        <RevelationCarte />
       </section>
       <section className="contenairSolution">
         <Solution />

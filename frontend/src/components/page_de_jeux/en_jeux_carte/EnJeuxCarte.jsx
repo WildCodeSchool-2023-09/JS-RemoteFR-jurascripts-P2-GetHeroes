b9d6981dd2bed5682d3615b933df019c333681ed
Blackstars <nelson.64@live.fr>
2023-11-03 10:30:17 +0100

    enfin reussi a recupéré le poucentage

--- frontend/src/components/page_de_jeux/en_jeux_carte/EnJeuxCarte.jsx
+++ frontend/src/components/page_de_jeux/en_jeux_carte/EnJeuxCarte.jsx
@@ -1,39 +1,15 @@
-import ScratchCard from "reactjs-scratchcard";
 import ReveltCard from "../../../assets/images/ReveltCard.png";
 import Hero from "../../../assets/images/test.jpg";
+import ScratchCard from "../../../util/ScratchCard";
 import "./enjeuxcarte.scss";
 
 function EnJeuxCarte() {
-  const handleComplete = () => {
-    // do some stuff here
-  };
-  const heightRevelt = "18.5rem";
-  const widthRevelt = "14rem";
-  const fadeOut = true;
   return (
-    <ScratchCard
-      brushSize={5}
-      fadeOutOnComplete={fadeOut}
-      finishPercent={76}
-      height={heightRevelt}
-      image={ReveltCard}
-      transitionProps={{ timeout: 200 }}
-      onComplete={handleComplete}
-      width={widthRevelt}
-    >
-      <span
-        style={{
-          width: "100%",
-          height: "100%",
-          textAlign: "center",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        <img src={Hero} alt="Hero" />
-      </span>
-    </ScratchCard>
+    <section className="contenairScratcard">
+      <ScratchCard width={224} height={288} image={ReveltCard} brushSize={5} />
+
+      <img src={Hero} alt="hero" />
+    </section>
   );
 }
 export default EnJeuxCarte;

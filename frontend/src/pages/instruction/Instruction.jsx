4449f5b605fc13828b9448ab307be823d123af85
Dricks45 <144217492+Dricks45@users.noreply.github.com>
2023-11-03 18:14:41 +0100

    modification du css de la page instructions

--- frontend/src/pages/instruction/Instruction.jsx
+++ frontend/src/pages/instruction/Instruction.jsx
@@ -3,21 +3,20 @@ import "./instruction.scss";
 function Instruction() {
   return (
     <section className="contenairInstruction">
-      <h2>Instruction</h2>
+      <h2>Instructions</h2>
       <p>Le but du jeu est de trouver ton héros le plus vite possible.</p>
       <p>
-        Le bouton Révéler vous permet de dévoiler le héros. Mais plus vous
-        révélerez le héros, moins vous gagnerez de Tokens.
+        En grattant la carte vous dévoilez le héros. Mais plus vous dévoilez le
+        héros, moins vous gagnerez de Tokens.
       </p>
       <p>
         Quand vous avez deviné le héros, écrivez-le et appuyez sur le bouton
         valider. Si c'est le bon héros, vous gagnerez la carte du héros, mais
-        aussi des token en fonction du nombre de fois que vous aurez pu le
-        révéler.
+        aussi les Tokens correspondant.
       </p>
       <p>
-        Vous pourrez aussi acheter des cartes des héros dans votre collection
-        grâce aux tokens.
+        Vous pourrez aussi acheter les cartes des héros dans votre collection
+        grâce aux Tokens.
       </p>
       <p className="last">
         Bonne partie et trouver le héros que vous appréciez le plus.

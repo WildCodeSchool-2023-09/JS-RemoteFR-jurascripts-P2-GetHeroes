3b4117f0c79b74f931a3db06670434173b040389
Dricks45 <144217492+Dricks45@users.noreply.github.com>
2023-11-05 01:06:33 +0100

    Création de la page zoom de la collection avec sont css

--- frontend/src/components/colector-zoom/ColectorZoom.jsx
+++ frontend/src/components/colector-zoom/ColectorZoom.jsx
@@ -1,12 +1,38 @@
 import ApiHeroes from "../../data/ApiHeroes";
 import "./colectorzoom.scss";
+import SVG from "../../assets/images/delete.svg";
 
 function ColectorZoom() {
   const apiHero = ApiHeroes();
+
   return (
-    <section className="zoom_bigcard">
-      <h1>zoom collection</h1>
-      <p>{apiHero[237].name} </p>
+    <section className="zoom-carte">
+      <img className="return-zoom" src={SVG} alt="Play" />
+      <p className="number-carte">237/481</p>
+      <img
+        className="img-hero-zoom"
+        src={apiHero[236]?.image.url}
+        alt={apiHero[236]?.name}
+        height="294"
+        width="230"
+      />
+      <p className="height-zoom desc">{apiHero[236]?.appearance.height[1]}</p>
+      <p className="weight-zoom desc">{apiHero[236]?.appearance.weight[1]}</p>
+      <h3 className="big-name-zoom">{apiHero[236]?.name}</h3>
+      <p className="name-zoom">
+        My name is {apiHero[236]?.name} alias{" "}
+        {apiHero[236]?.biography.aliases[0]}.
+      </p>
+      <p className="full-name-zoom">
+        My full name is {apiHero[236]?.biography["full-name"]}.
+      </p>
+      <p className="race-zoom">
+        I am a {apiHero[236]?.appearance.race} breed born in{" "}
+        {apiHero[236]?.biography["place-of-birth"]}.
+      </p>
+      <p className="publisher-zoom">
+        My publisher is {apiHero[236]?.biography.publisher}.
+      </p>
     </section>
   );
 }

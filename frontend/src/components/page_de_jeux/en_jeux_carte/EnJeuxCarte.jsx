9dfa94fe257eccb747e1ec71dd07d63965e1cbbe
Blackstars <nelson.64@live.fr>
2023-11-06 09:44:10 +0100

    amelioration de solution.jsx

--- frontend/src/components/page_de_jeux/en_jeux_carte/EnJeuxCarte.jsx
+++ frontend/src/components/page_de_jeux/en_jeux_carte/EnJeuxCarte.jsx
@@ -2,21 +2,33 @@ import ReveltCard from "../../../assets/images/ReveltCard.png";
 import ApiHeroes from "../../../data/ApiHeroes";
 import Rendomiser from "../../../util/Rendomiser";
 import ScratchCard from "../../../util/ScratchCard";
+import Solution from "../solution/Solution";
 import "./enjeuxcarte.scss";
 
 function EnJeuxCarte() {
-  const ApiHero = ApiHeroes();
-  const RendomiserApi = Rendomiser(ApiHero);
-  return (
-    <section className="contenairScratcard">
-      <ScratchCard width={224} height={288} image={ReveltCard} brushSize={5} />
+  const apiHero = ApiHeroes();
+  const rendomiserApi = Rendomiser(apiHero).slice(0, 1);
 
-      {RendomiserApi.map((hero, index) => (
-        <div key={index.id}>
-          <img src={hero.image.url} alt="hero" />
-        </div>
-      ))}
-    </section>
+  return (
+    <>
+      <section className="contenairScratcard">
+        <ScratchCard
+          width={224}
+          height={288}
+          image={ReveltCard}
+          brushSize={5}
+        />
+
+        {rendomiserApi.map((hero, index) => (
+          <div key={index.id}>
+            <img src={hero.image.url} alt={hero.name} />
+          </div>
+        ))}
+      </section>
+      <section className="contenairSolution">
+        <Solution rendomiserApi={rendomiserApi} />
+      </section>
+    </>
   );
 }
 export default EnJeuxCarte;
--- frontend/src/components/page_de_jeux/solution/Solution.jsx
+++ frontend/src/components/page_de_jeux/solution/Solution.jsx
@@ -1,12 +1,15 @@
+import PropTypes from "prop-types";
 import "./solution.scss";
 
-function Solution() {
+function Solution({ rendomiserApi }) {
   const handleSubmit = (e) => {
     e.preventDefault();
     const formData = new FormData(e.target);
     const heroValider = formData.get("valider");
-    // eslint-disable-next-line no-alert
-    alert(heroValider);
+    if (heroValider === rendomiserApi[0].name) {
+      // eslint-disable-next-line no-alert
+      alert("bravo");
+    }
   };
   return (
     <form onSubmit={handleSubmit}>
@@ -20,5 +23,11 @@ function Solution() {
     </form>
   );
 }
+
+Solution.propTypes = {
+  rendomiserApi: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+  }).isRequired,
+};
 
 export default Solution;

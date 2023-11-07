82d52025096eb949cba83b24dc2d81d4a891a576
Blackstars <nelson.64@live.fr>
2023-11-07 17:15:49 +0100

    api upgrade

--- frontend/src/data/ApiHeroes.jsx
+++ frontend/src/data/ApiHeroes.jsx
@@ -2,13 +2,28 @@ import { useState, useEffect } from "react";
 
 function ApiHeroes() {
   const [apidata, setApiData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
   useEffect(() => {
     fetch("https://www.superheroapi.com/api.php/24816519924613529/search/a")
-      .then((response) => response.json())
-      .then((data) => setApiData(data.results));
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setApiData(data.results);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setError(err);
+        setLoading(false);
+      });
   }, []);
 
-  return apidata;
+  return { apidata, loading, error };
 }
 
 export default ApiHeroes;

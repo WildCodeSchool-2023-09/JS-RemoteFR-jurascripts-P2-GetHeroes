9e43028b348ee206ac8b66fe441e578954d7e4d4
Dricks45 <144217492+Dricks45@users.noreply.github.com>
2023-11-09 00:16:27 +0100

    correction du franglais en anglais

---
+++ frontend/src/pages/in-game/InGame.jsx
@@ -0,0 +1,11 @@
+import InGameCard from "../../components/page_de_jeux/card-in-game/CardInGame";
+
+function InGame() {
+  return (
+    <section className="games-page-container">
+      <InGameCard />
+    </section>
+  );
+}
+
+export default InGame;

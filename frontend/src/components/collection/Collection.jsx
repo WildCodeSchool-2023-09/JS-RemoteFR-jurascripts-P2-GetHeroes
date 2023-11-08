5b996d2ebdff9499b47654513d216dacf4361891
Maxence Chevee <max.rothen@hotmail.fr>
2023-11-08 09:46:15 +0100

    debut caroussel

--- frontend/src/components/collection/Collection.jsx
+++ frontend/src/components/collection/Collection.jsx
@@ -1,17 +1,41 @@
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 import ApiHeroes from "../../data/ApiHeroes";
 import "./collection.scss";
 
 function Collection() {
-  const { heroes } = ApiHeroes();
+  const { apidata, loading } = ApiHeroes();
+  if (loading) {
+    return <div>chargement en cours...</div>;
+  }
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+  };
+
   return (
     <section className="cardList">
-      <div>
-        {heroes.map((hero) => (
+      <Slider
+        dots={settings.dots}
+        infinite={settings.infinite}
+        speed={settings.speed}
+        slidesToShow={settings.slidesToShow}
+        slidesToScroll={settings.slidesToScroll}
+      >
+        {apidata.map((hero) => (
           <div key={hero.id} className="border">
-            <img className="heroCard" src={hero.image.url} alt="name" />;
+            <img className="heroCard" src={hero.image.url} alt={hero.name} />
+            <h2>{hero.name}</h2>
           </div>
         ))}
-      </div>
+      </Slider>
     </section>
   );
 }

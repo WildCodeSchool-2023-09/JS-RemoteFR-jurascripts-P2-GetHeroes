9638ce2de11d6825fcd38fadabf1659cbc04228f
Blackstars <nelson.64@live.fr>
2023-11-09 18:01:44 +0100

    correction faite

--- frontend/src/pages/reward/Reward.jsx
+++ frontend/src/pages/reward/Reward.jsx
@@ -6,37 +6,35 @@ function Reward({ rendomiserApi, clickRestart }) {
     <section className="zoom-reward">
       <img
         className="img-hero-reward"
-        src={rendomiserApi[0].image.url}
-        alt={rendomiserApi[0].name}
+        src={rendomiserApi.image.url}
+        alt={rendomiserApi.name}
         height="294"
         width="230"
       />
-      <p className="height-reward desc">
-        {rendomiserApi[0].appearance.height[1]}
-      </p>
+      <p className="height-reward desc">{rendomiserApi.appearance.height[1]}</p>
       <p className="weight-reward desc">
-        {rendomiserApi[0].appearance.weight[1] === "0 kg"
+        {rendomiserApi.appearance.weight[1] === "0 kg"
           ? "--- kg"
-          : rendomiserApi[0].appearance.weight[1]}
+          : rendomiserApi.appearance.weight[1]}
       </p>
-      <h3 className="big-name-reward">{rendomiserApi[0].name}</h3>
+      <h3 className="big-name-reward">{rendomiserApi.name}</h3>
       <p className="name-reward">
-        Mon nom est {rendomiserApi[0].name}
-        {rendomiserApi[0].biography.aliases[0] === "-"
+        Mon nom est {rendomiserApi.name}
+        {rendomiserApi.biography.aliases === "-"
           ? "."
-          : `alias ${rendomiserApi[0].biography.aliases[0]}.`}
+          : `alias ${rendomiserApi.biography.aliases[0]}.`}
       </p>
       <p className="full-name-reward">
-        Je suis {rendomiserApi[0].biography["full-name"]}.
+        Je suis {rendomiserApi.biography["full-name"]}.
       </p>
       <p className="race-reward">
-        Je suis un {rendomiserApi[0].appearance.race}
-        {rendomiserApi[0].biography["place-of-birth"] === "-"
+        Je suis un {rendomiserApi.appearance.race}
+        {rendomiserApi.biography["place-of-birth"] === "-"
           ? "."
-          : `né à ${rendomiserApi[0].biography["place-of-birth"]}.`}
+          : `né à ${rendomiserApi.biography["place-of-birth"]}.`}
       </p>
       <p className="publisher-reward">
-        Mon éditeur est {rendomiserApi[0].biography.publisher}.
+        Mon éditeur est {rendomiserApi.biography.publisher}.
       </p>
       <button type="button" onClick={clickRestart}>
         Continuer
@@ -47,9 +45,12 @@ function Reward({ rendomiserApi, clickRestart }) {
 Reward.propTypes = {
   rendomiserApi: PropTypes.shape({
     name: PropTypes.string.isRequired,
+
     biography: PropTypes.shape({
       publisher: PropTypes.string.isRequired,
       aliases: PropTypes.string.isRequired,
+      "place-of-birth": PropTypes.string.isRequired,
+      "full-name": PropTypes.string.isRequired,
     }).isRequired,
     appearance: PropTypes.shape({
       race: PropTypes.string.isRequired,
